da45ff469e622b386c99eadc3586052db01454af
Abdalla Ahmed <abdallaahmed172125@gmail.com>
2025-02-04 15:32:05 +0300

    Let UnitOfWork handle transactions

--- ERP.POS.Domain/Common/IUnitOfWork/IUnitOfWork.cs
+++ ERP.POS.Domain/Common/IUnitOfWork/IUnitOfWork.cs
@@ -5,11 +5,17 @@ namespace ERP.POS.Domain.Common.IUnitOfWork
     public interface IUnitOfWork : IDisposable
     {
         IRepository<TEntity> Repository<TEntity>() where TEntity : class;
+
         Task SaveChangesAsync();
         void SaveChanges();
 
-        Task ExecuteInTransactionAsync(Func<Task> operation);
-        Task<T> ExecuteInTransactionAsync<T>(Func<Task<T>> operation);
-    }
+        void StartTransaction();
+        Task StartTransactionAsync();
 
+        void Commit();
+        Task CommitAsync();
+
+        void Rollback();
+        Task RollbackAsync();
+    }
 }
--- ERP.POS.Repository/UnitOfWork/UnitOfWork.cs
+++ ERP.POS.Repository/UnitOfWork/UnitOfWork.cs
@@ -2,69 +2,104 @@
 using ERP.POS.Domain.Common.IUnitOfWork;
 using ERP.POS.Repository.EntityFrameworkCore.Context;
 using ERP.POS.Repository.Repository;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace ERP.POS.Repository.UnitOfWork
 {
     public class UnitOfWork : IUnitOfWork
     {
         private readonly AppDbContext _context;
-        private readonly Dictionary<Type, object> _repositories = new();
+        private IDbContextTransaction? _currentTransaction;
+        private readonly Dictionary<Type, object> _repositories;
 
         public UnitOfWork(AppDbContext context)
         {
             _context = context;
+            _repositories = new Dictionary<Type, object>();
         }
 
         public IRepository<TEntity> Repository<TEntity>() where TEntity : class
         {
-            if (!_repositories.ContainsKey(typeof(TEntity)))
+            if (!_repositories.TryGetValue(typeof(TEntity), out var repository))
             {
-                _repositories[typeof(TEntity)] = new Repository<TEntity>(_context);
+                repository = new Repository<TEntity>(_context);
+                _repositories[typeof(TEntity)] = repository;
             }
-            return (IRepository<TEntity>)_repositories[typeof(TEntity)];
+            return (IRepository<TEntity>)repository;
         }
 
-        public async Task SaveChangesAsync() => await _context.SaveChangesAsync();
-
-        public void SaveChanges() => _context.SaveChanges();
-
-        public async Task ExecuteInTransactionAsync(Func<Task> operation)
+        public void SaveChanges()
         {
-            using var transaction = await _context.Database.BeginTransactionAsync();
-            try
-            {
-                await operation();
-                await _context.SaveChangesAsync();
-                await transaction.CommitAsync();
-            }
-            catch
-            {
-                await transaction.RollbackAsync();
-                throw;
-            }
+            _context.SaveChanges();
         }
 
-        public async Task<T> ExecuteInTransactionAsync<T>(Func<Task<T>> operation)
+        public async Task SaveChangesAsync()
         {
-            using var transaction = await _context.Database.BeginTransactionAsync();
-            try
-            {
-                T result = await operation();
-                await _context.SaveChangesAsync();
-                await transaction.CommitAsync();
-                return result;
-            }
-            catch
-            {
-                await transaction.RollbackAsync();
-                throw;
-            }
+            await _context.SaveChangesAsync();
+        }
+
+        public void StartTransaction()
+        {
+            if (_currentTransaction != null)
+                throw new InvalidOperationException("A transaction is already in progress.");
+
+            _currentTransaction = _context.Database.BeginTransaction();
+        }
+
+        public async Task StartTransactionAsync()
+        {
+            if (_currentTransaction != null)
+                throw new InvalidOperationException("A transaction is already in progress.");
+
+            _currentTransaction = await _context.Database.BeginTransactionAsync();
+        }
+
+        public void Commit()
+        {
+            if (_currentTransaction == null)
+                throw new InvalidOperationException("No active transaction to commit.");
+
+            _context.SaveChanges();
+            _currentTransaction.Commit();
+            _currentTransaction.Dispose();
+            _currentTransaction = null;
+        }
+
+        public async Task CommitAsync()
+        {
+            if (_currentTransaction == null)
+                throw new InvalidOperationException("No active transaction to commit.");
+
+            await _context.SaveChangesAsync();
+            await _currentTransaction.CommitAsync();
+            await _currentTransaction.DisposeAsync();
+            _currentTransaction = null;
+        }
+
+        public void Rollback()
+        {
+            if (_currentTransaction == null)
+                throw new InvalidOperationException("No active transaction to roll back.");
+
+            _currentTransaction.Rollback();
+            _currentTransaction.Dispose();
+            _currentTransaction = null;
+        }
+
+        public async Task RollbackAsync()
+        {
+            if (_currentTransaction == null)
+                throw new InvalidOperationException("No active transaction to roll back.");
+
+            await _currentTransaction.RollbackAsync();
+            await _currentTransaction.DisposeAsync();
+            _currentTransaction = null;
         }
 
         public void Dispose()
         {
             _context.Dispose();
+            _currentTransaction?.Dispose();
         }
     }
-
 }
